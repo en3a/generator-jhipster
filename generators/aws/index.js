9845a796ca2a3da91b2e70a5b83390f9a6719e0c
en3a <enea.dede@elitestudio.io>
2021-04-14 14:26:07 +0200

    updates

--- generators/aws/index.js
+++ generators/aws/index.js
@@ -161,6 +161,8 @@ module.exports = class extends BaseGenerator {
                 const cb = this.async();
                 this.log();
                 this.log(chalk.bold('Create database'));
+                this.log(chalk.bold(this.dbPassword));
+                this.log(chalk.bold(this.dbUsername));
 
                 const rds = this.awsFactory.getRds();
 
